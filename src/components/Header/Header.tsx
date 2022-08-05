61c64ca9a8842499d389c821edfb85f93ff47189
Flavio Jeronimo da Silva Filho <flaviofilho.js@gmail.com>
2022-08-05 18:45:29 -0300

    add barra de pesquisa

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,29 @@
+import { useState } from "react";
 import "./Header.css";
 
 export const Header = () => {
+  let [searchOnOff, setSearchOnOff] = useState([0, "0", "none"]);
+  let screenWidth = window.innerWidth;
+
   const mobileSearch = () => {
-    alert("ble ble");
+    if (searchOnOff[0] === 0) {
+      setSearchOnOff([1, "5.4rem", "block"]);
+    } else {
+      setSearchOnOff([0, "0", "none"]);
+    }
   };
+
   return (
     <header>
       <div className="header__top container">
         <div className="header__imgs">
           <img
-            className="header__img"
+            className="header__img-logo img-logo"
             src="/img/logo/logo-img.png"
             alt="logo da Hack Fashion"
           />
           <img
-            className="header__img header__img--display-none"
+            className="header__img-name img-name header__img--display-none"
             src="/img/logo/logo-name.png"
             alt="Hack Fashion"
           />
@@ -31,22 +40,63 @@ export const Header = () => {
         </nav>
       </div>
       <div className="header__bottom container header__bottom--display-none">
-        <div className="header__search ">
-          <input
-            id="header__input-text"
-            type="text"
-            name="search"
-            placeholder="Pesquise"
-          />
-          <i className="fa-solid fa-magnifying-glass"></i>
-        </div>
+        {screenWidth < 770 && (
+          <>
+            <div
+              className="header__search "
+              style={{ height: `${searchOnOff[1]}` }}
+            >
+              <input
+                id="header__input-text"
+                type="text"
+                name="search"
+                placeholder="Pesquise"
+                style={{
+                  opacity: `${searchOnOff[0]}`,
+                  display: `${searchOnOff[2]}`,
+                }}
+              />
+              <i
+                className="fa-solid fa-magnifying-glass"
+                style={{
+                  opacity: `${searchOnOff[0]}`,
+                  display: `${searchOnOff[2]}`,
+                }}
+              ></i>
+            </div>
+          </>
+        )}
+        {screenWidth >= 770 && (
+          <>
+            <div className="header__search " style={{ height: `5.4rem` }}>
+              <input
+                id="header__input-text"
+                type="text"
+                name="search"
+                placeholder="Pesquise"
+                style={{
+                  opacity: `1`,
+                  display: `block`,
+                }}
+              />
+              <i
+                className="fa-solid fa-magnifying-glass"
+                style={{
+                  opacity: `1`,
+                  display: `block`,
+                }}
+              ></i>
+            </div>
+          </>
+        )}
+
         <nav className="header__categories">
           <ul>
             <li>Jeans</li>
             <li>Camisas</li>
             <li>Suéter</li>
-            <li>Calças de noletom</li>
-            <li>Camisas de moletom</li>
+            <li>Calças moletom</li>
+            <li>Camisas moletom</li>
           </ul>
         </nav>
       </div>
